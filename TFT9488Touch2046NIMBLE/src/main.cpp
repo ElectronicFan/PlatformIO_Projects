d3e376202a6ebb74c787caf579e28a64ecd1ee93
Atari Fan <167288874+Issac567@users.noreply.github.com>
2026-05-01 20:12:26 -0700

    Remove commented project details and clean up code

--- TFT9488Touch2046NIMBLE/src/main.cpp
+++ TFT9488Touch2046NIMBLE/src/main.cpp
@@ -93,7 +93,6 @@ void loop() {
         if (connectToServer()) {
             Serial.println("We are now connected to the BLE Server.");
         } else {
-            doScan = false;
             Serial.println("We have failed to connect to the server; there is nothing more we will do.");
         }
     }
