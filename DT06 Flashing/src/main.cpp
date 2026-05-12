88c8104f15825fd5d8ce4b1af980d65b59637a51
Atari Fan <167288874+ElectronicFan@users.noreply.github.com>
2026-05-12 16:53:16 -0700

    Add files via upload

--- DT06 Flashing/src/main.cpp
+++ DT06 Flashing/src/main.cpp
@@ -1,4 +1,5 @@
-// Version 1.00
+// Version 1.01
+
 #include <ESP8266WiFi.h>
 
 WiFiServer server(8080); // B4J will connect to this port
@@ -7,9 +8,14 @@ WiFiClient client;
 // On DT-06/ESP8285, GPIO4 is typically the onboard LED
 #define LED_PIN 4
 
-void setup() {
+static uint8_t bridgeBuffer[2048];    
+static unsigned long lastSendTime = 0; 
+static size_t bufferIdx = 0;
+
+void setup() 
+{
 // 1. Boost the Buffer BEFORE Serial.begin
-  Serial.setRxBufferSize(1024); 
+  Serial.setRxBufferSize(2048); 
   Serial.begin(57600);
   delay(1500);
   
@@ -28,11 +34,13 @@ void setup() {
   Serial.println("WiFi Bridge Ready! Connect to 'GoBox_Bridge' with password '12345678");
 }
 
-void loop() {
+void loop() 
+{
   // Check for a NEW connection attempt
   WiFiClient newClient = server.accept(); // Use accept() instead of available()
   
-  if (newClient) {
+  if (newClient) 
+  {
     // If we already have a client, stop the old one to free up the socket
     if (client) 
     {
@@ -40,22 +48,29 @@ void loop() {
     }
     client = newClient;
     client.setNoDelay(true); 
-    Serial.println("B4J Connected!"); 
   }
 
-  // Ensure client is still valid before trying to move data
-  if (client && client.connected()) 
-  {
-    // 1. WiFi -> Serial (Dumb Pipe)
-    while (client.available() > 0) 
+  if (client && client.connected()) {
+    // Fill the buffer as fast as UART allows
+    while (Serial.available() > 0 && bufferIdx < (int)sizeof(bridgeBuffer))
     {
-      Serial.write(client.read());
+        bridgeBuffer[bufferIdx++] = Serial.read();
     }
 
-    // 2. Serial -> WiFi (Dumb Pipe)
-    while (Serial.available() > 0) {
-      client.write(Serial.read());
+    // Send if: Buffer is getting full OR 20ms has passed since last data
+    if (bufferIdx > 0) 
+    {
+        if (bufferIdx >= 256 || (millis() - lastSendTime > 20)) {
+            client.write(bridgeBuffer, bufferIdx);
+            bufferIdx = 0; // Reset index
+            lastSendTime = millis();
+        }
     }
-
+    
+    // WiFi to PIC should stay direct for the Cancel command
+    if (client.available() > 0) {
+        Serial.write(client.read());
+    }
   }
+
 }
