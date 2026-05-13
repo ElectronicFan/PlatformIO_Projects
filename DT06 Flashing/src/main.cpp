21236cb6ff2e34019529128744eac3c46ecba576
Atari Fan <167288874+ElectronicFan@users.noreply.github.com>
2026-05-12 18:07:45 -0700

    Add files via upload

--- DT06 Flashing/src/main.cpp
+++ DT06 Flashing/src/main.cpp
@@ -1,4 +1,4 @@
-// Version 1.01
+// Version 1.02
 
 #include <ESP8266WiFi.h>
 
@@ -8,69 +8,97 @@ WiFiClient client;
 // On DT-06/ESP8285, GPIO4 is typically the onboard LED
 #define LED_PIN 4
 
+// Buffer for bridging data from Serial to WiFi. Size can be adjusted based on needs and memory constraints.
 static uint8_t bridgeBuffer[2048];    
 static unsigned long lastSendTime = 0; 
 static size_t bufferIdx = 0;
 
+// For Flashing the LED when not connected, we will use millis() to create a non-blocking blink
+static unsigned long lastBlinkTime = 0;
+static bool ledState = false;
+
 void setup() 
 {
-// 1. Boost the Buffer BEFORE Serial.begin
-  Serial.setRxBufferSize(2048); 
-  Serial.begin(57600);
-  delay(1500);
-  
-  
-  // 2. Set CPU to Max Speed
-  WiFi.setOutputPower(20.5); // Maximize radio strength
-  WiFi.softAP("GoBox_Bridge", "12345678");
-  
-  client.setNoDelay(true); // Ensure this is set when client connects
-  server.begin();
+     // 1. Boost the Buffer BEFORE Serial.begin
+     Serial.setRxBufferSize(2048); 
+     Serial.begin(57600);
+     delay(1500);
+     
+     
+     // 2. Set CPU to Max Speed
+     WiFi.setOutputPower(20.5); // Maximize radio strength
+     WiFi.softAP("GoBox_Bridge", "12345678");
+     
+     client.setNoDelay(true); // Ensure this is set when client connects
+     server.begin();
 
-  // Turn on the LED to indicate we're ready (active LOW)
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+     // Turn on the LED to indicate we're ready (active LOW)
+     pinMode(LED_PIN, OUTPUT);
+     digitalWrite(LED_PIN, LOW);
 
-  Serial.println("WiFi Bridge Ready! Connect to 'GoBox_Bridge' with password '12345678");
+     Serial.println("WiFi Bridge Ready! Connect to 'GoBox_Bridge' with password '12345678");
 }
 
 void loop() 
 {
-  // Check for a NEW connection attempt
-  WiFiClient newClient = server.accept(); // Use accept() instead of available()
-  
-  if (newClient) 
-  {
-    // If we already have a client, stop the old one to free up the socket
-    if (client) 
-    {
-        client.stop();
-    }
-    client = newClient;
-    client.setNoDelay(true); 
-  }
+     // --- 1. Connection Management ---
+     WiFiClient newClient = server.accept(); // Use accept() instead of available()
+     
+     if (newClient) 
+     {
+     // If we already have a client, stop the old one to free up the socket
+     if (client) 
+     {
+          client.stop();
+     }
+          client = newClient;
+          client.setNoDelay(true); 
+          digitalWrite(LED_PIN, LOW);
+     }
 
-  if (client && client.connected()) {
-    // Fill the buffer as fast as UART allows
-    while (Serial.available() > 0 && bufferIdx < (int)sizeof(bridgeBuffer))
-    {
-        bridgeBuffer[bufferIdx++] = Serial.read();
-    }
+     // --- 2. Data Bridge (Active Connection) ---
+     if (client && client.connected()) 
+     {
 
-    // Send if: Buffer is getting full OR 20ms has passed since last data
-    if (bufferIdx > 0) 
-    {
-        if (bufferIdx >= 256 || (millis() - lastSendTime > 20)) {
-            client.write(bridgeBuffer, bufferIdx);
-            bufferIdx = 0; // Reset index
-            lastSendTime = millis();
-        }
-    }
-    
-    // WiFi to PIC should stay direct for the Cancel command
-    if (client.available() > 0) {
-        Serial.write(client.read());
-    }
-  }
+          // Fill the buffer as fast as UART allows
+          while (Serial.available() > 0 && bufferIdx < (int)sizeof(bridgeBuffer))
+          {
+               bridgeBuffer[bufferIdx++] = Serial.read();
+          }
+
+          // Send if: Buffer is getting full OR 20ms has passed since last data
+          if (bufferIdx > 0) 
+          {
+               if (bufferIdx >= 256 || (millis() - lastSendTime > 20)) 
+               {
+                    client.write(bridgeBuffer, bufferIdx);
+                    bufferIdx = 0; // Reset index
+                    lastSendTime = millis();
+               }
+          }
+     
+          // WiFi to PIC should stay direct for the Cancel command
+          if (client.available() > 0) 
+          {
+               Serial.write(client.read());
+          }
+
+
+     } else {
+          // --- 3. Heartbeat (Disconnected Mode) ---
+          // Blink every 500ms without using delay()
+          if (millis() - lastBlinkTime > 500) 
+          {
+               ledState = !ledState;
+               digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+               lastBlinkTime = millis();
+          }
+          
+          // Clear out any "junk" data from the PIC so the buffer doesn't 
+          // fill up while we are waiting for a connection
+          while(Serial.available() > 0) { Serial.read(); }
+          bufferIdx = 0; 
+
+     }
 
 }
